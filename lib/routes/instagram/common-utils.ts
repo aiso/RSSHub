efada25afc02a67efc6883da4ce61c8ca97910c5
aiso <aisobubu@gmail.com>
2026-06-14 03:44:17 +0800

    Filter videos by type in common-utils.ts

--- lib/routes/instagram/common-utils.ts
+++ lib/routes/instagram/common-utils.ts
@@ -24,7 +24,7 @@ const renderItems = (items) =>
             }
             case 'clips':
             case 'igtv':{
-                const sortedVideos=item.video_versions.filter(v=>v.height<=720).sort((a,b)=>b.height-a.height);
+                const sortedVideos=item.video_versions.filter(v=>v.type===101 && v.height<=720).sort((a,b)=>b.height-a.height);
                 const chosenVideo=sortedVideos[0]||item.video_versions[0];
                 description=renderVideo({
                     summary,
